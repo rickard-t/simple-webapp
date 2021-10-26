d7af364a521d3e10927635e4a78be40ccc255468
Rickard Torén <toren.rickard@gmail.com>
2021-10-26 18:05:05 +0200

    Use axios to make API calls from client
    Currently not working due to no CORS.

--- client/main.js
+++ client/main.js
@@ -1,11 +1,23 @@
 var app = new Vue({
   el: '#quote',
   data: {
-    message: '---'
+    api_url: 'https://zenquotes.io/api/random',
+    response: '---',
+    error: false,
+    loading: false
   },
   methods: {
     newQuote: function() {
-      this.message = 'Test'
+      this.error = false
+      this.loading = true
+      axios.get(this.api_url)
+        .then(response => (this.response = response))
+        .catch(error => {
+          console.log(error)
+          this.response = error
+          this.error = true
+        })
+        .finally(() => (this.loading = false))
     }
   }
 })
