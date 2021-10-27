550832ccf1fb1a185f47ed1a6f8301e27d80a79b
Rickard Torén <toren.rickard@gmail.com>
2021-10-27 15:48:20 +0200

    Better parse and display of the quote

--- client/main.js
+++ client/main.js
@@ -1,20 +1,35 @@
-var app = new Vue({
+var QUOTE_URL = '/quote'
+
+// Vue app for storing a quote and loading new quotes.
+var quote_app = new Vue({
   el: '#quote',
   data: {
-    api_url: '/quote',
-    response: '---',
+    quote: 'Press the button to show a new quote.',
+    author: 'Rickard Torén',
+    error_msg: '',
     error: false,
     loading: false
   },
   methods: {
+    // Loads a new quote from the local server and updates the webpage
     newQuote: function() {
       this.error = false
       this.loading = true
-      axios.get(this.api_url)
-        .then(response => (this.response = response))
+      axios.get(QUOTE_URL)
+        .then(response => {
+          // If the response does not contain the fields extracted,
+          // any error will be handled in the catch statement below.
+          data = response.data[0]
+          this.quote = data.q
+          this.author = data.a
+          if (this.quote == undefined || this.author == undefined) {
+            throw 'Error. Response is missing fields.'
+          }
+        })
+        // Catches both client and server errors
         .catch(error => {
           console.log(error)
-          this.response = error
+          this.error_msg = error
           this.error = true
         })
         .finally(() => (this.loading = false))
