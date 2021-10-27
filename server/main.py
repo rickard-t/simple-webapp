ced591d80c887f3ce6bb4d795e06b2d88d5a5a12
Rickard Torén <toren.rickard@gmail.com>
2021-10-27 14:53:24 +0200

    Use requests module to make API call to zenquotes

--- server/main.py
+++ server/main.py
@@ -1,16 +1,31 @@
 from flask import Flask, send_from_directory
+import requests
 
+QUOTE_URL = 'https://zenquotes.io/api/random'
 CLIENT_FOLDER = '../client'
 
+# Create the server and serve static files without adding extra folders in URL
 app = Flask(__name__, static_url_path='', static_folder=CLIENT_FOLDER)
 
 @app.route("/")
 def root():
+    """ Displays the home page under root URL. """
     return send_from_directory(CLIENT_FOLDER, 'index.html')
 
 @app.route("/quote")
 def quote():
-    return 'Server quote!'
+    """ Fetches a quote from the remote server and returns it. """
+    content = ''
+    status_code = 200
+    try:
+        response = requests.get(QUOTE_URL)
+        response.raise_for_status()
+        content = response.text
+    except requests.exceptions.RequestException as error:
+        print(error)
+        content = str(error)
+        status_code = 500
+    return content, status_code
 
 if __name__ == '__main__':
     app.run()
